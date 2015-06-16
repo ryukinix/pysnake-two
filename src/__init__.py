24cb3688b2b72f47276c1353af4731c55744f886
Manoel Vilela <manoel_vilela@engineer.com>
2015-06-16 05:06:33 -0300

    This file it's necessary to execute by main.py

---
+++ src/__init__.py
@@ -0,0 +1,19 @@
+# -*-  coding: utf-8  -*-
+# !/usr/bin/env python
+
+"""
+    -*- Python Studies -*- Snake Game Clone with Pygame -*-
+
+    Testing concepts of OO with so much gamb-code! : D
+
+    ==============================================
+
+             Author: Manoel Vilela M. Neto
+        Create Data: 29/01/2015
+           Revision: 0.1.3
+            License: Apache v2
+            Contact: manoel_vilela@engineer.com
+
+    ==============================================
+
+"""
